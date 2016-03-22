7b48c096de01e4387adc199757dec9ea190df95f
bbknightzm <bbknightzm@gmail.com>
2016-03-22 16:14:56 +0800

    Add scripting dir.

---
+++ scripting/javascript/bindings/js_bindings_opengl.h
@@ -0,0 +1,10 @@
+#include "CrossApp.h"
+#include "ScriptingCore.h"
+#include "CrossApp_specifics.hpp"
+//class CCNode;
+class GLNode : public CAView {
+ public:
+  void draw();
+};
+
+//void js_register_cocos2dx_GLNode(JSContext *cx, JSObject *global);
